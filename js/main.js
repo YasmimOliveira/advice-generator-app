d6f9648d2eb268cdd468426db07bbe2cbd183434
YasmimOliveira <yasmimmelissa7@gmail.com>
2022-09-06 21:31:51 -0300

    Ajustes de responsividade e agora é possível pesquisar por ID

--- js/main.js
+++ js/main.js
@@ -1,23 +1,58 @@
-document.addEventListener("DOMContentLoaded", () => {
-    main ();
-    document.getElementById("btn-advice").addEventListener("click", main);
-})
-
 var adviceTitle = document.querySelector(".advice-title");
 var adviceText = document.querySelector(".advice-text");
 
-function get (url) {
-    let request = new XMLHttpRequest();
-    request.open("GET", url, false);
-    request.send();
-    return request.responseText;
+document.addEventListener("DOMContentLoaded", () => {
+    generateAdvice();
+    document.getElementById("btn-advice").addEventListener("click", generateAdvice);
+    document.getElementById("btn-search").addEventListener("click", showSweetAlert );
+});
+
+function showSweetAlert() {
+    Swal.fire({
+        title: "Look for advice by it's ID",
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonColor: '#1f2632',
+        confirmButtonText: 'Search',
+        showLoaderOnConfirm: true,
+        preConfirm: (id) => {
+          return fetch(`https://api.adviceslip.com/advice/${id}`)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error(response.statusText)
+              }
+              return response.json()
+            })
+            .catch(error => {
+              Swal.showValidationMessage(
+                `Request failed: ${error}`
+              )
+            })
+        },
+      }).then((result) => {
+        if (result.isConfirmed) {
+            replaceHtml(result.value.slip)
+        }
+    })
 }
 
-function main () {
-    data = get('https://api.adviceslip.com/advice');
-    advicesObject = JSON.parse(data)
-    advicesId = advicesObject.slip.id
-    advicesAdvice = advicesObject.slip.advice;
-    adviceTitle.innerHTML = "ADVICE #" + advicesId;
-    adviceText.innerHTML = '"' + advicesAdvice + '"';
+function replaceHtml(data) {
+    id = data.id;
+    advice = data.advice;
+    adviceTitle.innerHTML = "ADVICE #" + id;
+    adviceText.innerHTML = '"' + advice + '"';
+}
+
+function generateAdvice() {
+    return fetch(`https://api.adviceslip.com/advice`).then(response => {
+            return response.json();
+    }).then (advice => {
+        replaceHtml(advice.slip)
+    })
+    .catch(error => {
+        console.log(error);
+    })
 }
